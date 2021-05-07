12ce6956c3dfcc37c1fbc6ecc479ae38cda1a91e
Etienne Kornobis <ekornobis@gmail.com>
2021-05-07 14:26:37 +0200

    Fighting with git and trying to update help ... git might win
    squash! For testing squash
    
    squash! Because black messed around ^^
    
    squash! Because I didn't realized how much black messed up the first time ...
    
    squash! This one is clearly just for fun :D
    
    squash! Can't get enough squashing ...

--- mergegi/cli.py
+++ mergegi/cli.py
@@ -12,7 +12,8 @@ from . import mergegi
     metavar='SAMPLESHEET',
     nargs=1,
     required=True,
-    help="CSV file with the samples names, barcodes, projects names and lane."
+    help="CSV file with the samples names, barcode1, barcode2, project names and lanes."
+    " The CSV should have a header and columns should be in this order.",
 )
 @click.option(
     '-i', '--input-directory',
@@ -20,7 +21,9 @@ from . import mergegi
     metavar='INPUT',
     nargs=1,
     required=True,
-    help="MGI output directory."
+    help="MGI directory where raw FastQ files (by lane) are to be found."
+    "Indeed, this directory should contain one directory per lane"
+    " (e.g. L01, L02, etc.). Ex: MGI/F301460/OutputFq/F301460",
 )
 @click.option(
     '-o', '--output-directory',
@@ -28,17 +31,45 @@ from . import mergegi
     metavar='OUTDIR',
     nargs=1,
     required=True,
-    help="Output directory with merged data."
+    help="Merged data output directory. The directory where the processed (merged) FastQ files will be stored.",
 )
 @click.option(
-    '-p', '--paired',
-    is_flag=True,
-    help="Set if your data are paired-end."
+    '-p', '--paired', is_flag=True, help="Set it if your data are paired-end."
 )
 @click.option(
     '--merge/--no-merge',
     default=True,
-    help="Disable it if you need data per lanes"
+    help="Disable it if you DO NOT want to merge data from different lanes and"
+    " keep the lane information in the processed FastQ files.",
 )
 def main(samplesheet, input_directory, output_directory, paired, merge):
+    """MergeGI is a command line tool to merge and organize sequencing data
+    (FastQ files) out of a MGI sequencer according to lanes and barcodes.
+
+    MGI DNBSEQ G400 sequencer produces FastQ files organized by
+    flowcell, lanes, barcodes and pairs. For example:
+
+    MGI/F300001460/OutputFq/F300001460/L01/F300001460_L01_22_1.fq.gz
+
+    Where F300001460 is the flowcell, L01 the lane, 22 the number of the barcode
+    and 1 shows that the FastQ corresponds to the first of the pair.
+
+    MergeGI make use of this information linked to the samplesheet to merge FastQ
+    files according to their barcodes, lanes and pairs. Barcode number will be replaced
+    by the corresponding sample names in the sample sheet.
+
+    Example:
+
+    If Single end sequencing::
+
+        mergegi -s samplesheet.csv -i mgi_fastq_folder -o merged_fastqs_folder
+
+    If paired end sequencing::
+
+        mergegi -s samplesheet.csv -i mgi_fastq_folder -o merged_fastqs_folder -p
+
+    If single end sequencing and not merging lanes::
+
+        mergegi -s samplesheet.csv -i mgi_fastq_folder -o merged_fastqs_folder --no-merge
+    """
     mergegi(samplesheet, input_directory, output_directory, paired, merge)
